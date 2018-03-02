106af29ead3e7e938f02b85d7ac28663582625cc
Tao Ong <taoong@berkeley.edu>
2018-03-02 14:04:39 -0800

    ListView added

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import TextablesScreen from '../Containers/TextablesScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  TextablesScreen: { screen: TextablesScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'TextablesScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
